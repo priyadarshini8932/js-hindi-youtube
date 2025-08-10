070b9dd4e05f5a52ea8856c4fa630ca4661e2647
priyadarshini8932 <shreyapriyadarshini032@gmail.com>
2025-08-10 07:18:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using nodejs, not browser
+console.log(3+3)//code readilibity should be high
+console.log("Hitesh")
+let name ="hitesh"
+let age =18 
+let isLoggedIn=false
+let state=null
+console.log(typeof "hitesh")
